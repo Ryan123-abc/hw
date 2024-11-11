5e813d1eae4668d1a88423870990e447de653678
ryan <ryan950109@gmail.com>
2024-11-11 12:48:26 +0800

    upd

--- hw3/Inside.c
+++ hw3/Inside.c
@@ -4,13 +4,13 @@ int main(){
 	int a, b;
 	printf("Please input a coordinate(x,y): ");
 	scanf("%d,%d", &a, &b);
-	if( -200==a || a==200 || -110==b || b==110 ){
+	if(a<-200 || 200<a || b<-110 || b>100){
+		printf("Outside\n");
+	}
+	else if( -200==a || a==200 || -110==b || b==110 ){
 		printf("On the boundary\n");
 	}
 	else if( -200 < a &&  a < 200  &&  -110 < b &&  b < 110 ){ 	
 		printf("Inside\n");
 	}	
-	else{
-		printf("Outside\n");
-	}
 }
