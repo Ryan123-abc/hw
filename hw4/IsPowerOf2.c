982ef33f05191427a4bc231f140bd903e2e6d8d8
ryan <ryan950109@gmail.com>
2024-11-14 16:07:04 +0800

    upd

--- hw4/IsPowerOf2.c
+++ hw4/IsPowerOf2.c
@@ -10,18 +10,16 @@ int main(){
 	}
 
 	if((num & (num - 1)) == 0){
-        int power = 0;
-        int temp = num;
+        	int power = 0;
+        	int temp = num;
         
-	while(temp > 1){
-            temp /= 2;
-            power++;
-        }
+		while(temp > 1){
+            		temp /= 2;
+            		power++;
+        	}
         
-	printf("%d is 2 to the power of %d.\n", num, power);
-   	
+		printf("%d is 2 to the power of %d.\n", num, power);
 	} 
-	
 	else{
         	printf("%d is not a power of 2.\n", num);
     	}
